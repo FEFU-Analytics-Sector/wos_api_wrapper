0f93f01e61c481673ab1d9539d4b6b24db8539ef
BLGALEX <lepehaleha@bk.ru>
2022-02-28 14:23:05 +1000

    correct paths

--- wos_api_wrapper/wos/utils/constants.py
+++ wos_api_wrapper/wos/utils/constants.py
@@ -1,32 +1,23 @@
-from os import environ
 from pathlib import Path
 
-# Paths for cached files
-if (Path.home()/".scopus").exists():
-    BASE_PATH = Path.home()/".scopus"
+if (Path.home()/".wos_api_wrapper").exists():
+    BASE_PATH = Path.home()/".wos_api_wrapper"
 else:
-    BASE_PATH = Path.home()/".pybliometrics"/"Scopus"
+    BASE_PATH = Path.home()/".wos_api_wrapper"/"Wos"
 DEFAULT_PATHS = {
     'QueryRetrieval': BASE_PATH/'query_retrieval',
     'ReferencesRetrieval': BASE_PATH/'references_retrieval',
 }
 
-
-# Configuration file location
-if 'PYB_CONFIG_FILE' in environ:
-    CONFIG_FILE = Path(environ['PYB_CONFIG_FILE'])
+if (Path.home()/".wos_api_wrapper").exists():
+    CONFIG_FILE = Path.home()/".wos_api_wrapper"/"config.ini"
 else:
-    if (Path.home()/".scopus").exists():
-        CONFIG_FILE = Path.home()/".scopus"/"config.ini"
-    else:
-        CONFIG_FILE = Path.home()/".pybliometrics"/"config.ini"
+    CONFIG_FILE = Path.home()/".wos_api_wrapper"/"config.ini"
 
-# URLs for all classes
 API_URL = 'https://wos-api.clarivate.com/api/wos/'
 URLS = {
     'UserQuerySearch': API_URL,
     'ReferencesRetrieval': API_URL + '/references',
 }
 
-# Limits for got data
 RETRIEVAL_MAX_RECORDS = 100
