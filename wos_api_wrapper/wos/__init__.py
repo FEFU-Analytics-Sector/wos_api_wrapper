e35379adb4325a5f6493783ba2e15e908dca951d
BLGALEX <lepehaleha@bk.ru>
2022-02-28 14:20:49 +1000

    imports added

---
+++ wos_api_wrapper/wos/__init__.py
@@ -0,0 +1,3 @@
+from wos_api_wrapper.wos.utils import *
+
+from wos_api_wrapper.wos.user_query_search import *
--- wos_api_wrapper/wos/utils/__init__.py
+++ wos_api_wrapper/wos/utils/__init__.py
@@ -1,2 +1,5 @@
 from wos_api_wrapper.wos.utils.constants import *
 from wos_api_wrapper.wos.utils.exception import *
+from wos_api_wrapper.wos.utils.startup import *
+from wos_api_wrapper.wos.utils.config_manager import *
+from wos_api_wrapper.wos.utils.patterns import *
