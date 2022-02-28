cb7bf34833fa282902226a1df520e9e33fd82534
BLGALEX <lepehaleha@bk.ru>
2022-02-28 14:26:01 +1000

    deleted startup file, dont use it

--- wos_api_wrapper/wos/utils/startup.py
+++
@@ -1,9 +0,0 @@
-from wos_api_wrapper.wos.utils.config_manager import ConfigManager
-
-
-config_manager = ConfigManager()
-try:
-    config = config_manager.get_or_create_config()
-    API_KEY = config.get('Authentication', 'APIKey')
-except EOFError:
-    pass
