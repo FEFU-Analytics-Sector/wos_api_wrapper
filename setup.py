b7798f145f0a0099ba1e112953d4982894489b99
BLGALEX <lepehaleha@bk.ru>
2022-03-03 17:17:29 +1000

    added setup files for pypi

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='wos_api_wrapper',
+      version='0.4.0',
+      description='Python package to access Web of Science API Expanded',
+      packages=['wos_api_wrapper'],
+      author_email='lepehaleha@yandex.ru',
+      zip_safe=False)
