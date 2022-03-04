d8f5f284df8c65b87253d507f979eecc0c31918b
BLGALEX <lepehaleha@bk.ru>
2022-03-04 16:38:13 +1000

    correct setupfile

--- setup.py
+++ setup.py
@@ -1,8 +1,24 @@
-from setuptools import setup
+import setuptools
 
-setup(name='wos_api_wrapper',
-      version='0.4.0',
-      description='Python package to access Web of Science API Expanded',
-      packages=['wos_api_wrapper'],
-      author_email='lepehaleha@yandex.ru',
-      zip_safe=False)
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="wos_api_wrapper",
+    version="0.4.4",
+    author="Aleksei Lepekha",
+    author_email="lepehaleha@yandex.ru",
+    description="Python package to access Web of Science API Expanded",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FEFU-Analytics-Sector/wos_api_wrapper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['requests',
+                      'simplejson'],
+    python_requires='>=3.6',
+)
